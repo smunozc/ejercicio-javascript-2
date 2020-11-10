45c4732ac0b7600506f017855ea53b3c1df544c0
smunozc <salvador.munoz0109@gmail.com>
2020-11-10 13:59:27 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ejercicio-javascript-2-y7ahd2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ejercicio-javascript-2-y7ahd2)
---
+++ script.js
@@ -0,0 +1,20 @@
+// En este ejercicio deberéis realizar algunos cambios sobre las dos listas incluidas en "index.html".
+// En la primera lista tendréis que hacer lo siguiente:
+//    * Añadid la clase "element-n" a cada "span" de la lista, siendo "n" el número del lugar que ocupa en la lista (por ejemplo el segundo "span" de la lista tendría la clase "element-2"). Para ello, haced uso de los selectores de nodo (parentNode, nextSibling, firstChild...) partiendo del "span" con la clase "selected" siempre.
+//    * Tras añadir las clases, haced uso de "querySelectorAll" para obtener solo los elementos "li" con valor par (teniendo en cuenta que el primer elmento es el 1) y, a continuación, eliminadlos.
+
+//En la segunda lista, que en principio está vacía, deberéis hacer esto:
+//    * Tenéis que generar dentro de esta lista, nodo a nodo, la misma estructura que ha quedado en la primera lista en la que realizastéis los cambios. Tiene que quedar igual, con la misma jerarquía y con las mismas clases, pero con la diferencia de que en vez de elementos "span" deben ser botones. Para replicar los elementos de la primera lista no hace falta que la recorráis, podéis simplemente generar cada elemento "a mano" una detrás de otro (aunque si usáis la lista de referencia para hacer algún tipo de bucle, mejor)
+//    * Después de generar esta segunda lista, añadid el atributo disabled al último botón.
+// Suerte!
+
+var selectedSpan = document.getElementsByClassName("selected");
+var list = selectedSpan.parentNode();
+console.log(list);
+//for (let i = 0; i < list.childNodes.length; i++) {}
+
+window.addEventListener("load", onLoad);
+
+function onLoad() {
+  console.log("hi");
+}
